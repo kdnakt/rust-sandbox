b3da992d56346e8ea685f8efe616f5c14b21ee39
kdnakt <a.kid.1985@gmail.com>
2024-07-07 15:38:25 +0900

    send dummy request

--- rusttls/src/main.rs
+++ rusttls/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+
+    let mut stream = TcpStream::connect("127.0.0.1:8443")?;
+    stream.write(&[7])?;
+    stream.read(&mut [0; 128])?;
+    Ok(())
 }
