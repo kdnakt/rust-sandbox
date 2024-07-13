e5e4fdaa3844d91b690fb61e31e71ab8a398d1de
kdnakt <a.kid.1985@gmail.com>
2024-07-13 15:08:58 +0900

    as_bytes()

--- rusttls/src/main.rs
+++ rusttls/src/main.rs
@@ -1,13 +1,24 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 
+use tls::TlsRecord;
+
 mod tls;
 
 fn main() -> std::io::Result<()> {
     println!("Hello, world!");
 
     let mut stream = TcpStream::connect("127.0.0.1:8443")?;
-    stream.write(&[7])?;
+
+    let record = TlsRecord {
+        content_type: tls::TlsContentType::Handshake,
+        // TLS 1.2
+        protocol_version: tls::TlsProtocolVersion { major: 3, minor: 3 },
+    };
+    println!("{:?}", record.content_type.as_bytes());
+    println!("{:?}", record.protocol_version.as_bytes());
+    println!("{:?}", record.as_bytes());
+    stream.write(record.as_bytes())?;
     stream.read(&mut [0; 128])?;
     Ok(())
 }
--- rusttls/src/tls.rs
+++ rusttls/src/tls.rs
@@ -1,17 +1,66 @@
-
-
+#[repr(C)]
 pub struct TlsRecord {
-    pub protocol_version: TlsProtocolVersion,
     pub content_type: TlsContentType,
+    pub protocol_version: TlsProtocolVersion,
 }
 
-pub enum TlsProtocolVersion {
-    TLS13,
+
+impl TlsRecord {
+    pub fn as_bytes(&self) -> &[u8] {
+        unsafe {
+            std::slice::from_raw_parts(
+                self as *const TlsRecord as *const u8,
+                std::mem::size_of::<TlsRecord>(),
+            )
+        }
+    }
 }
 
+#[repr(u8)]
 pub enum TlsContentType {
-    Handshake,
-    ApplicationData,
-    ChangeCipherSpec,
-    Alert,
+    Handshake = 22, // 0x16
+}
+
+impl TlsContentType {
+    pub fn as_bytes(&self) -> &[u8] {
+        unsafe {
+            std::slice::from_raw_parts(
+                self as *const TlsContentType as *const u8,
+                std::mem::size_of::<TlsContentType>(),
+            )
+        }
+    }
+}
+
+pub struct TlsProtocolVersion {
+    pub major: u8,
+    pub minor: u8,
+}
+
+impl TlsProtocolVersion {
+    pub fn as_bytes(&self) -> &[u8] {
+        unsafe {
+            std::slice::from_raw_parts(
+                self as *const TlsProtocolVersion as *const u8,
+                std::mem::size_of::<TlsProtocolVersion>(),
+            )
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tls12() {
+        let version = TlsProtocolVersion { major: 3, minor: 3 };
+        assert_eq!([3, 3], version.as_bytes());
+    }
+
+    #[test]
+    fn tls13() {
+        let version = TlsProtocolVersion { major: 3, minor: 4 };
+        assert_eq!([3, 4], version.as_bytes());
+    }
 }
