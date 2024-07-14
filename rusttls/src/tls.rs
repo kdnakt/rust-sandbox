6db2fb5aec952a195581cd37532e3a19c0fd7e9a
kdnakt <a.kid.1985@gmail.com>
2024-07-14 16:53:40 +0900

    fix as_bytes()

--- rusttls/src/tls.rs
+++ rusttls/src/tls.rs
@@ -12,7 +12,8 @@ impl<'a> TlsRecord<'a> {
         unsafe {
             std::slice::from_raw_parts(
                 self as *const TlsRecord as *const u8,
-                std::mem::size_of::<TlsRecord>(),
+                (5 + self.length).into(),
+                // std::mem::size_of::<TlsRecord>(),
             )
         }
     }
@@ -38,10 +39,10 @@ mod tests {
         let client_hello = TlsRecord {
             content_type: TlsContentType::Handshake,
             protocol_version,
-            length: 1,
-            data: &[0],
+            length: 3,
+            data: &[7, 8, 9],
         };
-        assert_eq!([22, 3, 3], client_hello.as_bytes());
+        assert_eq!([22, 3, 3, 0, 3, 7, 8, 9], client_hello.as_bytes());
     }
 
 }
