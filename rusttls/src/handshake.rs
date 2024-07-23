b6f33de991bb6010ed6314061d8e37cf7b31e82f
kdnakt <a.kid.1985@gmail.com>
2024-07-23 11:30:53 +0900

    legacy session id

--- rusttls/src/handshake.rs
+++ rusttls/src/handshake.rs
@@ -2,17 +2,20 @@ use crate::record::TlsProtocolVersion;
 
 
 pub enum TlsHandshake {
-    ClientHello(TlsProtocolVersion, [u8; 32])
+    ClientHello(TlsProtocolVersion, [u8; 32], [u8; 32])
 }
 
 impl TlsHandshake {
     pub fn as_bytes(&mut self) -> Vec<u8> {
         match self {
-            Self::ClientHello(version, random) => {
+            Self::ClientHello(version, random, legacy_session_id) => {
                 let mut vec: Vec<u8> = Vec::new();
                 vec.push(version.major);
                 vec.push(version.minor);
                 for i in random.into_iter() {
+                    vec.push(*i);
+                }
+                for i in legacy_session_id.into_iter() {
                     vec.push(*i);
                 }
                 vec
@@ -29,9 +32,13 @@ mod tests {
     fn client_hello() {
         let protocol_version = TlsProtocolVersion::tls1_2();
         let random: [u8; 32] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32];
-        let mut client_hello = TlsHandshake::ClientHello(protocol_version, random);
+        let legacy_session_id: [u8; 32] = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
+        let mut client_hello = TlsHandshake::ClientHello(protocol_version, random, legacy_session_id);
         let mut expected = vec![3, 3];
         for i in random.into_iter() {
+            expected.push(i);
+        }
+        for i in legacy_session_id.into_iter() {
             expected.push(i);
         }
         assert_eq!(expected, client_hello.as_bytes());
